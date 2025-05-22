386130b591c70029216968c7a30037265daccb20
ui-adiii <beaheroinyourlife@gmail.com>
2025-05-22 22:16:13 +0530

    day 1

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import Collection from './Pages/Collection'
+import About from './Pages/About'
+import Cart from './Pages/Cart'
+import Contact from './Pages/Contact'
+import Login from './Pages/Login'
+import Orders from './Pages/Orders'
+import PlaceOrder from './Pages/PlaceOrder'
+import Product from './Pages/Product'
+import Navbar from './Components/Navbar'
+import Footer from "./Components/Footer"
+import SearchBar from "./Components/SearchBar"
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <SearchBar/>
+      <Routes>
+       <Route path='/' element={<Home/>} />
+       <Route path='/collection' element={<Collection/>} />
+       <Route path='/about' element={<About/>} />
+       <Route path='/contact' element={<Contact/>} />
+       <Route path='/product/:prroductid' element={<Product/>} />
+       <Route path='/cart' element={<Cart/>} />
+       <Route path='/login' element={<Login/>} />
+       <Route path='/placeorder' element={<PlaceOrder/>} />
+       <Route path='/orders' element={<Orders/>} />
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/Context/ShopContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+import { products } from "../assets/assets";
+export const ShopContext = createContext();
+
+const ShopContextProvider = ({ children }) => {
+  const currency = "$";
+  const delivery_fee = 10;
+  const [search, setsearch] = useState("");
+  const [showSearch, setshowSearch] = useState(false);
+  const value = {
+    products,
+    currency,
+    delivery_fee,
+    search,
+    setsearch,
+    showSearch,
+    setshowSearch,
+  };
+  return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
+};
+export default ShopContextProvider;
